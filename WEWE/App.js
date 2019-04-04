6843444f14521bbf6f0290e4ac9dfe64c16a5b21
Andre1 <andreu.pc.1997@gmail.com>
2019-04-04 13:12:45 +0200

    holi

--- WEWE/App.js
+++ WEWE/App.js
@@ -6,24 +6,25 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = { text: '' }
+  onChange = (text) => {
+    this.setState({ text /*Es lo mateix que 'text: text' */ });
+  }
+  submit = (text) => {
+    alert(text);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput
+          onChangeText={this.onChange}
+          onSubmitEditing={this.submit} />
+        <Text style={styles.instructions}>Current text "{this.state.text}"</Text>
       </View>
     );
   }
