ebd5b1ab9ae01131d477d622ee1c7653a270b55a
Andre1 <andreu.pc.1997@gmail.com>
2019-06-09 12:06:40 +0200

    Update infoMovie.js

--- WEWE/infoMovie.js
+++ WEWE/infoMovie.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, Button, FlatList, TextInput, ImageBackground } from 'react-native';
 import { TouchableHighlight, TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 import axios from 'axios';
-
-
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class infoMovie extends Component {
     constructor(props) {
@@ -44,7 +43,7 @@ export default class infoMovie extends Component {
             let genres = this.state.infoFilm.genres;
             console.log(this.state.infoFilm);
             return ( 
-                <View>
+                <LinearGradient colors={['rgba(247,17,220,0.5)','rgba(247,17,220,0)']}>
                 <View style={styles.navBar2}>
                     <View style={styles.caja1}>
                         <Text style={styles.movies} >// MOVIES</Text>
@@ -57,12 +56,17 @@ export default class infoMovie extends Component {
                         <Image style={styles.logo}  source={require('./assets/wLogo.png')} />  
                     </TouchableOpacity>
                 </View>
-                <Image source={{uri: imglink + this.state.infoFilm.poster_path}} style={styles.imgBG}/>
-                <Text>{this.state.infoFilm.original_title}</Text>
-                {genres.map((genre) =>
-                <Text>{genre.name}</Text>
-                    )}
-            </View>
+                <View style={styles.imgBG}>
+                    <Image source={{uri: imglink + this.state.infoFilm.poster_path}} style={styles.imgBG}/>
+                    <Text style={styles.movieTitle}>{this.state.infoFilm.original_title}</Text>
+                </View>
+                <View style={styles.imgBGspace}></View>
+                <View style={styles.genresMovie}>
+                    {genres.map((genre,index) =>
+                    <Text key={index} style={styles.genreName}>{genre.name}</Text>
+                        )}
+                </View>
+            </LinearGradient>
              );
         }
        
@@ -72,7 +76,42 @@ export default class infoMovie extends Component {
 const styles = StyleSheet.create({
     imgBG: {
         width: '100%',
-        height: 150,
+        height: 200,
+        zIndex: 0,
+        position: 'absolute',
+    },
+    imgBGspace: {
+        width: '100%',
+        height: 120,
+        zIndex: 0,
+        position: 'relative',
+    },
+    movieTitle:{
+        color: 'white',
+        zIndex: 1,
+        position: 'absolute',
+        paddingBottom: 10,
+        paddingLeft: 10,
+        bottom: 0,
+        paddingTop: 10,
+        width: '100%',
+        backgroundColor: 'rgba(247,17,220,0.5)',
+        fontWeight: '900',
+        fontSize: 20,
+    },
+    genresMovie:{
+        zIndex: 0,
+        position: 'relative',
+        flexDirection: 'row',
+    },
+    genreName:{
+        color: 'white',
+        backgroundColor: 'rgba(117,117,117,0.5)',
+        borderRadius: 50,
+        padding: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        margin: 2,
     },
     navBar: {
         justifyContent: 'space-between',
@@ -91,7 +130,7 @@ const styles = StyleSheet.create({
       navBar2: {
         justifyContent: 'space-between',
         alignItems: 'stretch',
-        backgroundColor: 'rgba(11,233,199, 0.5)',
+        backgroundColor: 'rgba(247,17,220,0.5)',
         flexDirection: 'row',
         shadowColor: "#000",
         shadowOffset: {
@@ -101,6 +140,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.75,
         shadowRadius: 3.84,
         elevation: 5,
+        zIndex: 2,
       },
     caja1: {
         color: 'white',
